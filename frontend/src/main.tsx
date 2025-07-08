81bfc56674ba5fe2e2a794438675935e92c23830
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-08 17:00:18 -0300

    feat: adicionando a configuração global do react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { Toaster } from "./components/ui/sonner.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Toaster position="top-right" />
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Toaster position="top-right" />
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );

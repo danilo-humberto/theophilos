9291d196169a088d86dc5462aca1f322253b1e3a
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-09 16:23:22 -0300

    feat: adicionando a lógica de login já conectado com a api

--- frontend/src/components/forms/CardLogin.tsx
+++ frontend/src/components/forms/CardLogin.tsx
@@ -3,16 +3,52 @@ import HeaderForm from "./HeaderForm";
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
+import { useNavigate } from "react-router";
+import { useAuthQueries } from "@/hooks/useAuthQueries";
+import { setUserData } from "@/utils/storage";
+import { toast } from "sonner";
+import axios from "axios";
 
 const CardLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
+  const { loginMutation } = useAuthQueries();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    loginMutation.mutate(
+      { email, password },
+      {
+        onSuccess: (data) => {
+          const userData = {
+            access_token: data.access_token,
+            user: { ...data.user },
+          };
+          setUserData("user", userData);
+          toast.success("Login realizado com sucesso!");
+          navigate("/");
+        },
+        onError: (error) => {
+          if (axios.isAxiosError(error)) {
+            toast.error("Credenciais inválidas!");
+          } else {
+            toast.error("Ocorreu um erro inesperado, tente novamente.");
+          }
+        },
+      }
+    );
+  };
+
   return (
     <div className="bg-[#fff] rounded-sm w-full shadow-[2px_0px_15px_0px_rgba(0,0,0,0.1)] px-4">
       <HeaderForm
         title="Bem vindo de volta"
         description="Entre com suas credenciais para acessar a sua conta"
       />
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-2">
           <label htmlFor="email" className="font-semibold text-sm">
             Email
@@ -23,6 +59,8 @@ const CardLogin = () => {
             id="email"
             placeholder="Insira seu email"
             className="border border-sidebar-border rounded-sm p-2 w-full text-sm"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -40,6 +78,8 @@ const CardLogin = () => {
               name="password"
               id="password"
               placeholder="Insira sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="text-sm outline-none w-full border-none pr-2"
             />
             {showPassword ? (

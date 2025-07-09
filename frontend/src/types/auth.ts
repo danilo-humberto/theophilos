5d7ac147e6cf4a5872bf514c099e7a7645e9288e
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-09 16:24:27 -0300

    feat: adicionando os tipos das requisições

---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,25 @@
+export interface LoginDTO {
+  email: string;
+  password: string;
+}
+
+export interface RegisterDTO {
+  name: string;
+  email: string;
+  password: string;
+  role?: string;
+}
+
+export interface VerifyEmailDTO {
+  email: string;
+  code: string;
+}
+
+export interface AuthResponse {
+  access_token: string;
+  user: {
+    id: string;
+    name: string;
+    role: string;
+  };
+}

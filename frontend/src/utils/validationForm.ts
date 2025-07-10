bb98f5a5c8c11ddb066b5fb67686bf4fa3b481a2
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-10 15:13:34 -0300

    fix: removendo a validação de confirmPassword

--- frontend/src/utils/validationForm.ts
+++ frontend/src/utils/validationForm.ts
@@ -1,19 +1,13 @@
 import { z } from "zod";
 
-export const userSchema = z
-  .object({
-    name: z.string().min(1, "O nome é obrigatório"),
-    email: z.string().email("Email inválido").min(1, "O email é obrigatório"),
-    password: z
-      .string()
-      .min(6)
-      .regex(
-        /[a-zA-Z][0-9]/,
-        "A senha precisa conter, no mínimo 6 caracters, com letras e números"
-      ),
-    confirmPassword: z.string(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "As senhas não coincidem",
-    path: ["confirmPassword"],
-  });
+export const userSchema = z.object({
+  name: z.string().min(1, "O nome é obrigatório"),
+  email: z.string().email("Email inválido").min(1, "O email é obrigatório"),
+  password: z
+    .string()
+    .min(6)
+    .regex(
+      /[a-zA-Z][0-9]/,
+      "A senha precisa conter, no mínimo 6 caracters, com letras e números"
+    ),
+});

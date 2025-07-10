78662d926a24e959e6865e1adbe065de76930baf
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-10 15:15:19 -0300

    feat: trazendo a lógica de login

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,10 +1,55 @@
 import CardLogin from "@/components/forms/CardLogin";
+import { useAuthQueries } from "@/hooks/useAuthQueries";
+import { setUserData } from "@/utils/storage";
+import VerificationStorage from "@/utils/verificationStorage";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
 
 const Login = () => {
+  const { loginMutation } = useAuthQueries();
+  const navigate = useNavigate();
+
+  const handleSubmit = (
+    e: React.FormEvent,
+    email: string,
+    password: string
+  ) => {
+    e.preventDefault();
+
+    loginMutation.mutate(
+      { email, password },
+      {
+        onSuccess: (data) => {
+          const userData = {
+            access_token: data.access_token,
+            user: { ...data.user },
+          };
+          setUserData("user", userData);
+          toast.success("Login realizado com sucesso!");
+          navigate("/");
+        },
+        onError: (error) => {
+          if (axios.isAxiosError(error)) {
+            toast.error("Credenciais inválidas!");
+          } else {
+            toast.error("Ocorreu um erro inesperado, tente novamente.");
+          }
+        },
+      }
+    );
+  };
+
   return (
-    <div className="w-full h-screen flex items-center justify-center p-4 md:max-w-[25%] m-auto">
-      <CardLogin />
-    </div>
+    <>
+      <VerificationStorage />
+      <div className="w-full h-screen flex items-center justify-center p-4 md:max-w-[25%] m-auto">
+        <CardLogin
+          onSubmit={handleSubmit}
+          isPending={loginMutation.isPending}
+        />
+      </div>
+    </>
   );
 };
 

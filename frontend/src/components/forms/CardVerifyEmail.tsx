f90bff95a38b6474bdded551a0fd07e3c2ad716c
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-10 15:11:16 -0300

    feat: controlando o design do button enquanto está verificando o código

--- frontend/src/components/forms/CardVerifyEmail.tsx
+++ frontend/src/components/forms/CardVerifyEmail.tsx
@@ -1,10 +1,22 @@
-import { Mail } from "lucide-react";
+import { Loader2, Mail } from "lucide-react";
 import { Button } from "../ui/button";
 import { useState } from "react";
 import { Separator } from "../ui/separator";
 import { useLocation } from "react-router";
 
-const CardVerifyEmail = () => {
+interface Props {
+  onSubmit: (e: React.FormEvent, email: string, code: string) => void;
+  onResendSubmit: (e: React.FormEvent, email: string) => void;
+  isPending?: boolean;
+  isPendingResend?: boolean;
+}
+
+const CardVerifyEmail = ({
+  onSubmit,
+  isPending,
+  onResendSubmit,
+  isPendingResend,
+}: Props) => {
   const [code, setCode] = useState("");
   const location = useLocation();
   const { email } = (location.state as { email: string }) || {};
@@ -48,17 +60,30 @@ const CardVerifyEmail = () => {
       <Button
         variant="destructive"
         className="w-full"
-        disabled={code.length != 6}
+        disabled={code.length != 6 || isPending}
+        onClick={(e) => {
+          onSubmit(e, email.trim(), code.trim());
+          setCode("");
+        }}
       >
-        Verificar Email
+        {isPending && <Loader2 className="animate-spin" />}Verificar Email
       </Button>
 
       <Separator className="mt-4" />
 
       <div className="flex flex-col gap-2 text-sm items-center mt-4">
         <p className="text-muted-foreground">Não recebeu o código?</p>
-        <Button variant="outline" className="w-full">
-          Reenviar
+        <Button
+          variant="outline"
+          className="w-full"
+          onClick={(e) => onResendSubmit(e, email.trim())}
+          disabled={isPendingResend}
+        >
+          {isPendingResend ? (
+            <Loader2 className="animate-spin" />
+          ) : (
+            <>Reenviar</>
+          )}
         </Button>
       </div>
     </div>

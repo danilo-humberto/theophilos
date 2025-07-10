5e2efd28b33eb6ca519ebc0f076a85ab5e6c904a
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-10 15:14:33 -0300

    fix: passando a role por props

--- frontend/src/views/AllRaffles.tsx
+++ frontend/src/views/AllRaffles.tsx
@@ -1,12 +1,14 @@
 import ApresentationAllRafflesCard from "@/components/allRaffles/ApresentationAllRafflesCard";
 import Filters from "@/components/Filters";
+import { getUserData } from "@/utils/storage";
 
 const AllRaffles = () => {
+  const user = getUserData("user");
   return (
     <div className="flex flex-col gap-4">
       <Filters />
       <ApresentationAllRafflesCard
-        role="SELLER"
+        role={user?.user?.role || "SELLER"}
         raffles={[
           {
             name: "rifa 1",

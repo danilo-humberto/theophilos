9e25ca724709289611641a559c9344e64c2dfb97
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-10 15:10:11 -0300

    fix: tirando o input de confirmPassword

--- frontend/src/components/forms/CardRegister.tsx
+++ frontend/src/components/forms/CardRegister.tsx
@@ -1,14 +1,14 @@
-import { Eye, EyeOff, Lock, Mail, User } from "lucide-react";
+import { Eye, EyeOff, Loader, Lock, Mail, User } from "lucide-react";
 import HeaderForm from "./HeaderForm";
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
+import { Link } from "react-router";
 
 interface UsersErrors {
   name?: string;
   email?: string;
   password?: string;
-  confirmPassword?: string;
 }
 
 interface CardRegisterProps {
@@ -21,6 +21,7 @@ interface CardRegisterProps {
   errors: UsersErrors;
   onChange: (field: string, value: string) => void;
   onSubmit: (e: React.FormEvent) => void;
+  isPending?: boolean;
 }
 
 const CardRegister = ({
@@ -28,9 +29,9 @@ const CardRegister = ({
   errors,
   onChange,
   onSubmit,
+  isPending,
 }: CardRegisterProps) => {
   const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   return (
     <div className="bg-[#fff] rounded-sm w-full shadow-[2px_0px_15px_0px_rgba(0,0,0,0.1)] px-4">
       <HeaderForm
@@ -119,45 +120,8 @@ const CardRegister = ({
             </span>
           )}
         </div>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="confirmPassword" className="font-semibold text-sm">
-            Confirmar Senha
-          </label>
-          <div className="border border-sidebar-border rounded-sm p-2 w-full flex justify-between">
-            <Lock size={18} className="text-muted-foreground" />
-            <input
-              type={showPassword ? "text" : "password"}
-              name="confirmPassword"
-              id="confirmPassword"
-              placeholder="Confirme a sua senha"
-              className="w-full px-2 text-sm outline-none border-none"
-              value={userData.confirmPassword}
-              onChange={(e) => onChange("confirmPassword", e.target.value)}
-            />
-            {showConfirmPassword ? (
-              <EyeOff
-                className="cursor-pointer text-muted-foreground"
-                onClick={() => setShowConfirmPassword(false)}
-                width={20}
-                height={20}
-              />
-            ) : (
-              <Eye
-                className="cursor-pointer text-muted-foreground"
-                onClick={() => setShowConfirmPassword(true)}
-                width={20}
-                height={20}
-              />
-            )}
-          </div>
-          {errors.confirmPassword && (
-            <span className="text-destructive text-[12px] px-2">
-              {errors.confirmPassword}
-            </span>
-          )}
-        </div>
-        <Button variant="destructive" onClick={onSubmit}>
-          Criar conta
+        <Button variant="destructive" onClick={onSubmit} disabled={isPending}>
+          {isPending && <Loader className="animate-spin" />} Criar Conta
         </Button>
       </form>
 
@@ -165,9 +129,9 @@ const CardRegister = ({
 
       <div className="flex text-[14px] justify-center mb-4">
         <p className="text-muted-foreground">Já possui uma conta?</p>
-        <a href="/login" className="text-destructive ml-1">
+        <Link to="/login" className="text-destructive ml-1">
           Entrar
-        </a>
+        </Link>
       </div>
     </div>
   );

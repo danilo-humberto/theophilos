b78d3171287be6ffd32e5bc046bb68179125abfb
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-10 15:13:19 -0300

    fix: ajustando o type para ficar do formato correto que está indo para o localStorage

--- frontend/src/utils/storage.ts
+++ frontend/src/utils/storage.ts
@@ -1,10 +1,19 @@
 const prefixKey = "@auth/";
 
+type User = {
+  access_token: string;
+  user: {
+    id: string;
+    name: string;
+    role: string;
+  };
+};
+
 export const setUserData = (key: string, value: any) => {
   localStorage.setItem(prefixKey + key, JSON.stringify(value));
 };
 
-export const getUserData = (key: string) => {
+export const getUserData = (key: string): User | null => {
   const data = localStorage.getItem(prefixKey + key);
   return data ? JSON.parse(data) : null;
 };

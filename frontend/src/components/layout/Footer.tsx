5df269f21f747c2afa43622bdb44bfa6215e925e
Danilo Humberto <danilohumberto12@hotmail.com>
2025-07-21 08:26:34 -0300

    fix: fixando o footer ao final da página

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center p-2 text-muted-foreground text-[12px]">
+    <footer className="flex items-center justify-center p-2 text-muted-foreground text-[12px] fixed bottom-0 w-full">
       &copy; {new Date().getFullYear()} Theophilos. Todos os direitos
       reservados.
     </footer>
